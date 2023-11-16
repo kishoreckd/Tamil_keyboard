692243ea35ed9dae9ee79384655b1ac7b9ae0bfc
Kishore Kumar <kishorekumarcdckap@gmail.com>
2023-11-16 13:13:35 +0530

    firebase added

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:keyboard_tamil/screens/keyboard.dart';
 
 // ignore_for_file: avoid_print
 
-void main() => runApp(const MaterialApp(home: TamilKeyboard()));
+// void main() => runApp(const MaterialApp(home: TamilKeyboard()));
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(const MaterialApp(
+    home: TamilKeyboard(),
+  ));
+}
